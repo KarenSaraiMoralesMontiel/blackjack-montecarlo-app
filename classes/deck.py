4fcbf66358f7a9b560cd0e49d5b0aedc7551b762
Kai Mm <karen.sarai.morales@gmail.com>
2024-04-02 13:55:04 -0600

    Added DocsStrings to Deck Class

--- classes/deck.py
+++ classes/deck.py
@@ -6,6 +6,10 @@ class Deck:
         self.shuffle_deck()
 
     def shuffle_deck(self):
+        """Creates Deck full of Cards
+        
+            Returns nothing
+        """
         suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
         values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
         for suit in suits:
@@ -13,7 +17,12 @@ class Deck:
                 self.cards.append(Card(suit, value))
         random.shuffle(self.cards)
 
-    def deal_card(self):
+    def deal_card(self) -> Card:
+        """Returns a car from the top of the deck (at the end)
+
+        Returns:
+            Card: The last card object in the deck
+        """
         if len(self.cards) == 0:
             self.shuffle_deck()
         return self.cards.pop()
